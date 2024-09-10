2574f1f1322cb117a79e93c77618b49d42d146d1
aakash1526 <aakashks640@gmail.com>
2024-09-10 12:07:44 +0530

    test msg

---
+++ xtable/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import XTable from './XTable';
+
+const App = () => {
+  return (
+    <div className="App">
+      <XTable />
+    </div>
+  );
+};
+
+export default App;
